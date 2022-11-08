02013f0eb13976a51872e11592d98b24ebfd72f9
Valentin Lepiller <vlepille@e2r12p22.clusters.42paris.fr>
2022-11-08 18:56:10 +0100

    Continue

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,13 +6,29 @@
 /*   By: vlepille <vlepille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 02:27:19 by vlepille          #+#    #+#             */
-/*   Updated: 2022/11/08 04:11:09 by vlepille         ###   ########.fr       */
+/*   Updated: 2022/11/08 18:14:18 by vlepille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
 // @TODO keep size_t or transform to unsigned int
-void	*ft_memmove(void *dest, const void *src, long unsigned int n);
+void    *ft_memmove(void *dst, const void *src, size_t len)
+{
+    size_t                i;
+    unsigned char        *pdst;
+    const unsigned char    *psrc;
 
+    pdst = (unsigned char *)dst;
+    psrc = (unsigned char *)src;
+    i = 0;
+    if (psrc < pdst)
+        while (++i <= len)
+            pdst[len - i] = psrc[len - i];
+    else
+        while (len-- > 0)
+            *(pdst++) = *(psrc++);
+    return (dst);
+}
 
 // @TODO DEL
 // ******************TESTS*******************
@@ -21,12 +37,11 @@ void	*ft_memmove(void *dest, const void *src, long unsigned int n);
 #include <assert.h>
 #include <string.h>
 #include <stdio.h>
-#include <stddef.h>
 
-void	*ft_memmove(void *dest, const void *src, long unsigned int n)
-{
-	return (memmove(dest, src, n));
-}
+//void	*ft_memmove(void *dest, const void *src, long unsigned int n)
+//{
+//	return (memmove(dest, src, n));
+//}
 
 void	test_memmove(int n)
 {
@@ -62,6 +77,24 @@ void	test_memmove(int n)
 		assert(dest89[i] == dest289[i]);
 	for (int i = 0; i < 5; i++)
 		assert(new89[i] == new289[i]);
+
+	char	src878[] = "ZICAPITAMIXTAPE";
+	char	src2878[] = "ZICAPITAMIXTAPE";
+	char	*dest878 = src878;
+	char	*dest2878 = src2878;
+	char	*new878 = memmove(dest878, src878, n);
+	char	*new2878 = ft_memmove(dest2878, src2878, n);
+	for (int i = 0; i < 5; i++)
+		assert(dest878[i] == dest2878[i]);
+	for (int i = 0; i < 5; i++)
+		assert(new878[i] == new2878[i]);
+
+	//char	*src007 = NULL;
+	//char	*src2007 = NULL;
+	//char	*dest007 = NULL;
+	//char	*dest2007 = NULL;
+	//ft_memmove(dest2007, src2007, n);
+	//memmove(dest007, src007, n);
 }
 
 int	main(void)

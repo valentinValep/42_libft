7ba5897b263e8111a44ceaeb2a9ca6e5ed5788cd
Valentin Lepiller <vlepille@e1r10p19.clusters.42paris.fr>
2022-11-16 19:14:48 +0100

    Continue returning

--- README.md
+++
@@ -1,8 +0,0 @@
-# TODO
-## At the end
-- NORMINETTE and valgrind
-- check each malloc protection and free() (expecially free when problem)
-- rm -README.md Libft_subject.pdf test.c libft.h
-- remove test rule of Makefile
-- check every file for @TODO
-- check every static
--- ft_itoa.c
+++ ft_itoa.c
@@ -6,11 +6,12 @@
 /*   By: vlepille <vlepille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 16:51:22 by vlepille          #+#    #+#             */
-/*   Updated: 2022/11/16 16:51:24 by vlepille         ###   ########.fr       */
+/*   Updated: 2022/11/16 19:12:48 by vlepille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
 int	int_len(int n)
 {
@@ -26,13 +27,15 @@ char	*ft_itoa(int n)
 	char	*res;
 	int		i;
 
-	i = int_len(n);
-	res = malloc((i + 1) * sizeof(char));
+	i = int_len(n) - 1 - (n < 0);
+	res = malloc((i + 2 + (n < 0)) * sizeof(char));
+	if (!res)
+		return (0);
 	n < 0 && (res[0] = '-', 1);
-	res[i] = 0;
-	while (n != 0)
+	res[i + 1 + (n < 0)] = 0;
+	while (i >= 0)
 	{
-		res[i - 1] = '0' + (n % 10) * ((n > 0) * 2 - 1);
+		res[i + (n < 0)] = '0' + (n % 10) * ((n > 0) * 2 - 1);
 		n = n / 10;
 		i--;
 	}

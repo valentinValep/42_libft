8e598410cae2cbb07c005b6523a8877195fbd516
Valentin Lepiller <vlepille@e2r8p2.clusters.42paris.fr>
2022-11-21 11:03:15 +0100

    Optimize ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: vlepille <vlepille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 21:03:40 by vlepille          #+#    #+#             */
-/*   Updated: 2022/11/15 15:16:33 by vlepille         ###   ########.fr       */
+/*   Updated: 2022/11/21 11:02:19 by vlepille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,16 @@ void	*ft_memset(void *pointer, int value, size_t size)
 {
 	if (!size)
 		return (pointer);
-	*(char *)pointer = (char) value;
-	ft_memset(pointer + 1, value, size - 1);
+	while (size > 0)
+		((char *)pointer)[--size] = (char) value;
 	return (pointer);
 }
+
+//void	*ft_memset(void *pointer, int value, size_t size)
+//{
+//	if (!size)
+//		return (pointer);
+//	*(char *)pointer = (char) value;
+//	ft_memset(pointer + 1, value, size - 1);
+//	return (pointer);
+//}
